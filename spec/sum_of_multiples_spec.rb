a0f60644ea8cf84acb4353f6bb187321240ddf23
Ashwatth Nagpal <ashwatthnagpal@Ashwatths-MacBook-Pro-2.local>
2019-01-15 00:02:49 +0530

    test condition to check sum of multiples of (4,6)  with limit till 15

--- spec/sum_of_multiples_spec.rb
+++ spec/sum_of_multiples_spec.rb
@@ -22,7 +22,11 @@ describe "SumOfMultiples" do
     expect(s.to(1000)).to eq 233168
   end
   it "one multiples within limits" do
-    s = SumOfMultiples.new(7,13,17)
+    s = SumOfMultiples.new(7, 13, 17)
     expect(s.to(20)).to eq 51
+  end
+  it "one multiples within limits" do
+    s = SumOfMultiples.new(4, 6)
+    expect(s.to(15)).to eq 30
   end
 end
