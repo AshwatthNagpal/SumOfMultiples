0ce8381a56c655935f6807f1ed8a1dc3b7181538
Ashwatth Nagpal <ashwatthnagpal@Ashwatths-MacBook-Pro-2.local>
2019-01-15 00:24:49 +0530

    test condition to check sum of multiples of (5,25)  with limit till 51

--- spec/sum_of_multiples_spec.rb
+++ spec/sum_of_multiples_spec.rb
@@ -1,36 +1,40 @@
 require "sum-of_multiples"
 
 describe "SumOfMultiples" do
-  it "No multiples within limits" do
+  it "sum of multiiples of (3,5) within limit 1" do
     s = SumOfMultiples.new(3, 5)
     expect(s.to(1)).to eq 0
   end
-  it "one multiples within limits" do
+  it "sum of multiples of (3,5) within limit 4" do
     s = SumOfMultiples.new(3, 5)
     expect(s.to(4)).to eq 3
   end
-  it "one multiples within limits" do
+  it "sum of multiple of 4 within limit 7" do
+    s = SumOfMultiples.new(3)
+    expect(s.to(7)).to eq 9
+  end
+  it "sum of multiples of (3,5) within limit 10" do
     s = SumOfMultiples.new(3, 5)
     expect(s.to(10)).to eq 23
   end
-  it "one multiples within limits" do
+  it "sum of multiples of (3,5) within limit 100" do
     s = SumOfMultiples.new(3, 5)
     expect(s.to(100)).to eq 2318
   end
-  it "one multiples within limits" do
+  it "sum of multiples of (3,5) within limit 1000" do
     s = SumOfMultiples.new(3, 5)
     expect(s.to(1000)).to eq 233168
   end
-  it "one multiples within limits" do
+  it "sum of multiples of (7,13,17) within limit 20" do
     s = SumOfMultiples.new(7, 13, 17)
     expect(s.to(20)).to eq 51
   end
-  it "one multiples within limits" do
+  it "sum of multiples of (4,6) within limit 30" do
     s = SumOfMultiples.new(4, 6)
     expect(s.to(15)).to eq 30
   end
-  it "one multiples within limits" do
-    s = SumOfMultiples.new(5, 6, 8)
-    expect(s.to(150)).to eq 4419
+  it "sum of multiples of (5,25) within limit 51" do
+    s = SumOfMultiples.new(5, 25)
+    expect(s.to(51)).to eq 275
   end
 end
